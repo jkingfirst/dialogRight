af0ca419ad986b06839e6a8217c8ddab54868731
jkingfirst <jinyuanchengking@sina.com>
2018-11-17 10:48:27 +0800

    update

--- dialogRight.js
+++ dialogRight.js
@@ -1,56 +1,10 @@
 ;(function($,window,document,undefinded){
-    /* function DialogRight(opt){
-     this.default={
-     width:600,
-     height:'100%',
-     title:'hello word',
-     content:'内容区',
-     url:'',
-     }
-     this.settings = $.extend({},this.default,opt)
-     }
-     DialogRight.prototype={
-     init:function(){
-     var self = this,
-     html = '<div class="dialogRight"> <div class="dialogMask"></div> <div class="dialogContent animate fadeInRight" style="height:1000px;"></div> </div>',
-     $dialogRight = $('.dialogRight');
-     if($dialogRight.length>0){$dialogRight.remove()}else{
-     $(html).append('body')
-     }
-     }
-     }
-     $.fn.DialogRight = function(opt){
-     var dialogRight = new DialogRight(opt)
-     return dialogRight.init()
-     }*/
-    /*    $.extend({
-     DialogRight:function(opt){
-     var defaults = {
-     width:600,
-     height:'100%',
-     title:'hello word',
-     content:'内容区',
-     url:'',
-     },
-     html = '<div class="dialogRight"> <div class="dialogMask"></div> <div class="dialogContent animate fadeInRight" style="height:1000px;"></div> </div>',
-     $dialogRight = $('.dialogRight');
-     var settings = $.extend({},defaults,opt);
-     if($dialogRight.length>0){$dialogRight.remove()}else{
-     $('body').append(html)
-     }
-     $(document).on('click',function(){
-     $dialogRight.remove();
-     console.log(1)
-     })
-     }
-     })*/
     $.DialogRight={
         init:function(opt){
             var defaults = {
                     title:'hello word',
+                    url:'',
                     content:'',
-                    url:'https://baidu.com',
-                    btns:[]
                 },
                 html = '<div class="dialogRight"> <div class="dialogMask"></div> <div class="dialogContent animate fadeInRight"><div class="dialogTitle"></div><div class="dialogCon"></div><div class="dialogFooter"><input class="dialog-cancel" type="button" value="" /><input class="dialog-commit" type="button" value=""/></div></div></div> </div>',
                 $dialogRight =$('.dialogRight');
@@ -68,10 +22,23 @@
                     var ifr = '<iframe name="ifrcon" frameborder="0" id="ifrcon" src='+settings.url+'></iframe>'
                     $dialogCon.append($(ifr))
                 }
-                if(settings.btns){
+                if(settings.btns.length!==0){
                     $dialogContent.find('input').css({'display':'inline-block'})
                     if(settings.btns[0]!=''){
-
+                      $('.dialog-cancel').attr('value',settings.btns[0])
+                    }
+                    var $cancel = $('.dialog-cancel');
+                    $cancel.on('click',function(){
+                        settings.cancel && settings.cancel();
+                        $.DialogRight.close()
+                    })
+                    if(settings.btns[1]!=''){
+                        $('.dialog-commit').attr('value',settings.btns[1])
+                        var $commit = $('.dialog-commit');
+                        $commit.on('click',function(){
+                            settings.commit && settings.commit();
+                            $.DialogRight.close()
+                        })
                     }
                 }else{
                     $dialogContent.find('input').css({'display':'none'})
